a8a8f4fa81f1e223e6816a57a39e9268165d8a42
Albert Biju <161057291+AlbertBi7@users.noreply.github.com>
2024-10-02 15:25:10 +0530

    Update WriteReadBinary.java

--- Tutorial/Tutorial6/WriteReadBinary.java
+++ Tutorial/Tutorial6/WriteReadBinary.java
@@ -3,31 +3,41 @@ import java.io.*;
 
 public class WriteReadBinary {
 
-	public static void main(String[] args) {
-		File f=new File("binary.dat");
+	public static void main(String[] args) throws IOException {
+
 		String s;
 		try {
-			FileOutputStream fo=new FileOutputStream("binary.dat");
-			s="HELOO WORLD!";
+			FileOutputStream fo=new FileOutputStream("binaryInput.dat");
+			s="MAN WILL IF HE CAN";
 			byte[] c=s.getBytes();
 			
 			fo.write(c);
+			fo.close();
 		} catch (IOException e) {
 			
 			e.getMessage();
 		}
+		
 		try {
-			FileInputStream fi=new FileInputStream("binary.dat");
+			FileInputStream fi=new FileInputStream("binaryInput.dat");
+			FileOutputStream fo=new FileOutputStream("binaryOutput.dat");
+			
 			int i;
 			while((i=fi.read())!=-1){
-				System.out.print((char)i);
+				fo.write(i);
 			}
+			fi.close();
+			fo.close();
+			System.out.println("File copied Successfuly");
 		} catch (IOException e) {
 			
 			e.getMessage();
 		}
+		
+		
 	}
 }
+
 
 
 
